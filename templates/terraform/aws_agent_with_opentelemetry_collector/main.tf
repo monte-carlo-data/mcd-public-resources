36e6ff9256586aed5d105568eee00e0b3aa6f2f5
Gene Hynson <ghynson@montecarlodata.com>
2025-09-23 13:24:48 -0700

    chore: update terraform module reference to 0.2.0

--- templates/terraform/aws_agent_with_opentelemetry_collector/main.tf
+++ templates/terraform/aws_agent_with_opentelemetry_collector/main.tf
@@ -43,7 +43,7 @@ module "agent" {
 # OpenTelemetry Collector Module
 module "opentelemetry_collector" {
   source  = "monte-carlo-data/otel-collector/aws"
-  version = "0.1.1"
+  version = "0.2.0"
 
   deployment_name                = var.deployment_name
   existing_vpc_id                = var.existing_vpc_id
